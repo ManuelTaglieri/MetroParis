4dd08b565ecc1a1009f1976ba3a48eb6ea50e38a
Manuel Taglieri <s257476@studenti.polito.it>
2021-05-03 11:07:58 +0200

    Calcolo dei cammini minimi completato

--- src/main/java/it/polito/tdp/metroparis/model/TestModel.java
+++ src/main/java/it/polito/tdp/metroparis/model/TestModel.java
@@ -1,5 +1,7 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.List;
+
 public class TestModel {
 
 	public static void main(String[] args) {
@@ -7,6 +9,19 @@ public class TestModel {
 		Model m = new Model();
 		
 		m.creaGrafo();
+		
+		Fermata p = m.trovaFermata("La Fourche");
+		if (p==null) {
+			System.out.println("Fermata non trovata");
+		} else {
+		List<Fermata> raggiungibili = m.fermateRaggiungibili(p);
+		System.out.println(raggiungibili);
+		}
+		
+		Fermata a = m.trovaFermata("Temple");
+		
+		List<Fermata> percorso = m.trovaCammino(p, a);
+		System.out.println(percorso);
 
 	}
 
